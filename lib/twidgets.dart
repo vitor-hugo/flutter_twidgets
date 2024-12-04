36ccfd0954666ddee38c71bbaf1aece3af086919
vitor-hugo <vitor83dev@gmail.com>
2024-12-04 16:20:48 -0300

    init: exporting TButton widget

---
+++ lib/twidgets.dart
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 Vitor Hugo de Oliveira. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+library twidgets;
+
+export 'widgets/button.dart';
