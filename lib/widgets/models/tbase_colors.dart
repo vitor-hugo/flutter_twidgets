4364ba009bbcdbb8fe67fc016c113769a68b8a27
vitor-hugo <vitor83dev@gmail.com>
2024-12-27 08:20:57 -0300

    init: TBaseColors class

---
+++ lib/widgets/models/tbase_colors.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2024 Vitor Hugo de Oliveira. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class TBaseColors {
+  TBaseColors({
+    this.background,
+    this.foreground,
+    this.icon,
+    this.border,
+    this.shadow,
+  });
+
+  Color? background;
+  Color? foreground;
+  Color? icon;
+  Color? border;
+  Color? shadow;
+}
